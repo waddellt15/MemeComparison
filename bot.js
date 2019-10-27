83c84fd3ac581a406dac9f7eb0e315c230661802
waddellt15 <waddellt15@gmail.com>
2019-10-26 22:24:54 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ var cool = require('cool-ascii-faces');
 var botID = "0b09c5795270482bb28ecfb5ef";
 
 function respond() {
-  console.log(request);
+  console.log(this.req.chunks[0]);
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^\/cool guy$/;
 
