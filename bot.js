6a06ba541954f9d0525893f372aa1b09abe27b36
waddellt15 <waddellt15@gmail.com>
2019-10-26 20:37:31 -0500

    Fixing

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 var HTTPS = require('https');
 var cool = require('cool-ascii-faces');
 
-var botID = process.env.botTest;
+var botID = process.env.botTest.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
