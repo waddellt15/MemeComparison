6251dc3c07c1d329df56a89a9847ccb78d6c4b01
waddellt15 <waddellt15@gmail.com>
2019-10-28 02:44:36 -0500

    testing

--- bot.js
+++ bot.js
@@ -83,7 +83,7 @@ function newPhoto() {
 
 }
 function findAllMessages(messageID) {
-    HTTPS.get('https://api.groupme.com/v3/groups/55230894/messages?before=' + messageID + '&limit=100&token=c2b94360da7f013732bc364efad1a7ec', function (res) {
+    HTTPS.get('https://api.groupme.com/v3/groups/55230894/messages?before=' + messageID + '&limit=10&token=c2b94360da7f013732bc364efad1a7ec', function (res) {
         if (res.statusCode == 200) {
             //neat
         } else {
