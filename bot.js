6979d8754be027a60077a830dfb08d79c077ffad
waddellt15 <waddellt15@gmail.com>
2019-10-27 23:15:01 -0500

    First real update

--- bot.js
+++ bot.js
@@ -5,17 +5,73 @@ var fs = require('fs');
 var botID = "0b09c5795270482bb28ecfb5ef";
 
 function respond() {
+    var request = JSON.parse(this.req.chunks[0]);
+
+    if (request.text == "/clarkstart") {
+        initiateFile();
+        this.res.writeHead(200);
+        this.res.end();
+    } else if (Array.isArray(request.attachments) && request.attachments.length) {
+        newPhoto();
+        this.res.writeHead(200);
+        this.res.end();
+    } else {
+        console.log("don't care");
+        this.res.writeHead(200);
+        this.res.end();
+    }
+}
+function initiateFile() {
     // configure aws and start process
     AWS.config.update({ accessKeyId: process.env.AWS_ACCESS_KEY_ID, secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY });
     var s3 = new AWS.S3();
     //download our main file
     var request = HTTPS.get("https://groupmeclark3000.s3.us-east-2.amazonaws.com/newfile.txt", function (err) {
-        
+
+    });
+    // creating empty data file
+    var data = '';
+    returnState = '';
+    //get all the messages
+    HTTPS.get('https://api.groupme.com/v3/groups/55230894/messages?limit=100&token=c2b94360da7f013732bc364efad1a7ec', function (res) {
+        if (res.statusCode == 200) {
+            //neat
+        } else {
+            console.log('rejecting bad status code ' + res.statusCode);
+        }
+        //add the chunks to our var data
+        res.on('data', function (chunk) {
+            data += chunk;
+        });
+        // on end iterate through file
+        res.on('end', function () {
+            var mess = JSON.parse(data).response.messages;
+            for (i = 0; i < mess.length; i++) {
+                if (mess[i].attachments.length) {
+                    for (j = 0; j < mess[i].attachments.length; j++) {
+                        if (mess[i].attachments[j].type == "image") {
+                            returnState += mess[i].attachments[j].url;
+                            returnState += ",";
+                            returnState += mess[i].created_at;
+                            returnState += ",";
+                            returnState += mess[i].nanme;
+                            returnState += "\n";
+                            console.log(mess[i].attachments[j].url);
+                            console.log(mess[i].created_at);
+                        }
+                    }
+                }
+                if (i == mess.length - 1) {
+                    console.log(mess[i].id);
+                }
+            }
+        });
+
     });
     //write to our main filel
-    fs.appendFile('newfile.txt', 'more to the file', function (err) {
+    fs.appendFile('newfile.txt', returnState, function (err) {
         if (err) throw err;
-        console.log('File is saved');
+        console.log('file is edited.');
     });
     // read out main file, convert it into bas64Data and then upload as text file
     fs.readFile('newfile.txt', function (err, data) {
@@ -31,62 +87,10 @@ function respond() {
             console.log('Successfully uploaded package.');
         });
     });
-    //get the request data, convert it into json
-    var request = JSON.parse(this.req.chunks[0]);
-    //check if it has an attachment
-    if (Array.isArray(request.attachments) && request.attachments.length) {
-        var att = request.attachments[0].url;
-        console.log(att);
-        //group me api settings
-        options = {
-            hostname: 'api.groupme.com',
-            path: '/v3/groups',
-            method: 'GET'
-        };
-        // creating empty data file
-        var data = '';
-        //get all the messages
-        HTTPS.get('https://api.groupme.com/v3/groups/55230894/messages?limit=100&token=c2b94360da7f013732bc364efad1a7ec', function (res) {
-            if (res.statusCode == 200) {
-                //neat
-            } else {
-                console.log('rejecting bad status code ' + res.statusCode);
-            }
-            //add the chunks to our var data
-            res.on('data', function (chunk) {
-                data += chunk;
-            });
-            // on end iterate through file
-            res.on('end', function () {
-                var mess = JSON.parse(data).response.messages;
-                for (i = 0; i < mess.length; i++) {
-                    if (mess[i].attachments.length) {
-                        for (j = 0; j < mess[i].attachments.length; j++) {
-                            if (mess[i].attachments[j].type == "image") {
-                                console.log(mess[i].attachments[j].url);
-                                console.log(mess[i].created_at);
-                            }
-                        }
-                    }
-                    if (i == mess.length - 1) {
-                        console.log(mess[i].id);
-                    }
-                }
-            });
-
-        });
-
-    }
-    if (request.text) {
-        this.res.writeHead(200);
-        this.res.end();
-    } else {
-        console.log("don't care");
-        this.res.writeHead(200);
-        this.res.end();
-    }
 }
+function newPhoto() {
 
+}
 function postMessage() {
     var botResponse, options, body, botReq;
 
