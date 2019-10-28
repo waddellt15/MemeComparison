e137f6eb8ba6faf55d2856cec5025125a798bce4
waddellt15 <waddellt15@gmail.com>
2019-10-27 22:52:32 -0500

    hot fix

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ function respond() {
     AWS.config.update({ accessKeyId: process.env.AWS_ACCESS_KEY_ID, secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY });
     var s3 = new AWS.S3();
     //download our main file
-    var request = http.get("https://groupmeclark3000.s3.us-east-2.amazonaws.com/newfile.txt", function (response) {
+    var request = HTTPS.get("https://groupmeclark3000.s3.us-east-2.amazonaws.com/newfile.txt", function (response) {
         response.pipe(file);
     });
     //write to our main filel
