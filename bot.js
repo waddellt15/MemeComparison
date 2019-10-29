af5ddafc127c8fa2d7080bc35362858824c55d5b
waddellt15 <waddellt15@gmail.com>
2019-10-29 01:44:46 -0500

    t

--- bot.js
+++ bot.js
@@ -179,7 +179,7 @@ function findAllMessages(messageID) {
             }
             console.log(mess[mess.length - 1].id);
             console.log(messageID);
-            findAllMessages(mess[mess.length - 1].id, 30000);
+            setTimeout(findAllMessages(mess[mess.length - 1].id),30000);
         });
 
     });
