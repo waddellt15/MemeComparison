2234930d5c46e44b36c278c9d092f3f327d02c50
waddellt15 <waddellt15@gmail.com>
2019-10-28 22:23:03 -0500

    t

--- bot.js
+++ bot.js
@@ -105,7 +105,7 @@ function pushData() {
     var s3 = new AWS.S3();
     //download our main file
     HTTPS.get("https://groupmeclark3000.s3.us-east-2.amazonaws.com/newfile.txt", function (err) {
-        if (err) throw err;
+        //if (err) throw err;
     });
     var params = {
         Bucket: process.env.S3_BUCKET_NAME,
