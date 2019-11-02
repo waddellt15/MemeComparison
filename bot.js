0b53e8239240558dfebb558fab4bfa6ac3a005a9
waddellt15 <waddellt15@gmail.com>
2019-11-02 17:46:15 -0500

    output

--- bot.js
+++ bot.js
@@ -300,10 +300,8 @@ function hashing(url) {
                     if (!err) console.log("we did it");
                     uploadfile()
                     PNG.decode('reformat.png', function (pixels) {
-                        var arr = [pixels];
-                        var array = pixels;
-                        console.log(arr);
-                        console.log(array);
+                        var ui32 = new Uint32Array(pixels.buffer, pixels.byteOffset, pixels.byteLength / Uint32Array.BYTES_PER_ELEMENT);
+                        console.log(ui32);
                     });
                 });
         }, 20);
