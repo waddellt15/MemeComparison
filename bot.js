92c0030824f49ec074172ee6d9e9b8fd7c27a2a1
waddellt15 <waddellt15@gmail.com>
2019-11-02 18:04:05 -0500

    test

--- bot.js
+++ bot.js
@@ -318,9 +318,7 @@ function hashing(url) {
                             }
                         }
                         console.log(Hashn);
-                        console.log(parseInt(Hashn, 16))
-                        console.log(parseInt(Hashn, 10))
-                        console.log(parseInt(Hashn, 2))
+                        console.log(parseInt(Hashn, 10).toString(16))
                     });
                 });
         }, 20);
