e918103626bf3577ffc94520657680c4249260e8
waddellt15 <waddellt15@gmail.com>
2019-12-28 23:40:11 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -374,7 +374,7 @@ function hashing(url) {
             gm(request(url))
 			    .noProfile()
 				.colorspace('Rec709Luma')
-				.filter('Bessel')
+				.filter('Sinc')
 				.unsharp(0, 5,2)
                 .resize(size+1, size, '!')
                 //.crop(size,size,0,0)
