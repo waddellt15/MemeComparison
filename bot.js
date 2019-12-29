e7ae022dbc6616c9d92482a389f427314585a104
waddellt15 <waddellt15@gmail.com>
2019-12-29 01:26:14 -0600

    Mitchell

--- bot.js
+++ bot.js
@@ -374,7 +374,7 @@ function hashing(url) {
             gm(request(url))
 			    .noProfile()
 				.colorspace('Rec709Luma')
-				.filter('Lanczos') //Catrom worked pretty well. Sinc worked decent. Bessel is awful.
+				.filter('Mitchell') //Catrom worked pretty well. Sinc worked decent. Bessel is awful. Lanczos not great. 
 				//.unsharp(0, 4,3)
                 .resize(size+1, size, '!')
                 //.crop(size,size,0,0)
