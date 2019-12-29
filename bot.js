e8c8dbe76d57c071e22eec3dde8612eb3a98d6bd
waddellt15 <waddellt15@gmail.com>
2019-12-29 01:36:00 -0600

    Adding sharp back

--- bot.js
+++ bot.js
@@ -375,7 +375,7 @@ function hashing(url) {
 			    .noProfile()
 				.colorspace('Rec709Luma')
 				.filter('Cubic') //Catrom worked pretty well. Sinc worked decent. Bessel is awful. Lanczos not great. Mitchell not bad.
-				//.unsharp(0, 4,3)
+				.unsharp(0, 4,3)
                 .resize(size+1, size, '!')
                 //.crop(size,size,0,0)
                 .write('reformat.png', function (err) {
